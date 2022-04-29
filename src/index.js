5b1fe992c373e561b0f17128cab89e89b137c8ff
Rohitsch <contact.rohitsch@gmail.com>
2022-04-29 13:40:03 +0530

    19 april

--- src/index.js
+++ src/index.js
@@ -8,22 +8,36 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 
-mongoose.connect("mongodb+srv://functionup-cohort:G0Loxqc9wFEGyEeJ@cluster0.rzotr.mongodb.net/Pritesh8769811-DB?retryWrites=true&w=majority", {
-    useNewUrlParser: true
-})
-.then( () => console.log("MongoDb is connected"))
-.catch ( err => console.log(err) )
+mongoose.connect("mongodb+srv://Rohitsch:S*Crohit16@cluster0.31aen.mongodb.net/rohitsch?retryWrites=true&w=majority", {
+        useNewUrlParser: true
+    })
+    .then(() => console.log("MongoDb is connected"))
+    .catch(err => console.log(err))
 
-app.use (
-    function (req, res, next) {
-        console.log ("inside GLOBAL MW");
+
+//app.use(
+//    function(req, res, next) {
+//        console.log("inside GLOBAL MW");
+//        next();
+//  }
+//);
+
+app.use(
+    function(req, res, next) {
+        let date = new Date
+        console.log(date)
+        console.log(req.ip)
+        console.log(req.path)
+
+
         next();
-  }
-  );
+    });
+
+
 
 app.use('/', route);
 
 
-app.listen(process.env.PORT || 3000, function () {
+app.listen(process.env.PORT || 3000, function() {
     console.log('Express app running on port ' + (process.env.PORT || 3000))
 });
